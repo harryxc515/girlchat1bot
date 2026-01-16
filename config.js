8a53f8ecb2fd9b7f87e5035e35a0b99e6256aeb4
harryxc515 <harryxc515@gmail.com>
2026-01-16 18:52:05 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+export const config = {
+  model: "gpt-4o-mini",
+  maxTokens: 280,
+  temperature: 0.7,
+
+  memoryLimit: 10,
+  spamLimit: 6,
+  spamWindowMs: 8000,
+
+  groupReplyOnlyTag: true
+};
